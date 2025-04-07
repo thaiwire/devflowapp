06f2580a4920a765e08739829a14538536e94000
thaiwireproducts <thaiwireproducts1@gmail.com>
2025-04-07 16:47:37 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next"; 
+import localFont from "next/font/local"
+import "./globals.css";
+
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900"
+});
+
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700"
+});
+ 
+
+export const metadata: Metadata = {
+  title: "Easy DevFlow",
+  description: "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons:{
+    icon: "/images/site-logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="h1-bold text-primary-500">Welcome To My Course </h1>
+      <h1 className="h2-bold text-primary-500">Welcome To My Course </h1>
+      <h1 className="h2-semibold text-primary-500">Welcome To My Course </h1>
+      <h1 className="h3-bold text-primary-500">Welcome To My Course </h1>
+      
+      
+      
+      <Link href='/about'>
+      <h1 className="h2-bold">About </h1>
+      </Link>
+     
+   </div>
+  );
+}
